4e855715e98d09b0ec1e167d9de0d97b162763f0
Gabriela Von <102772336+G4bizinha@users.noreply.github.com>
2022-06-10 00:17:56 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@FILA-HOSPITALAR-1406/main.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h> 
+#include <string.h>
+#include <unistd.h>
+
+int controleMenu=1;
+
+// sistema de cores
+#define VERDE "\x1b[32m"  
+#define AMARELO "\x1b[93m"
+#define VERMELHO "\x1b[31m"
+#define AZUL "\x1b[34m"
+#define RESET "\x1b[0m"
+  
+  struct Paciente{
+   	int valor;
+  	char nome[20];
+		char sobrenome[20];
+		char prioridade;
+   struct Node *prox;
+  };
+
+typedef struct Paciente paciente;
+
+void inserir(paciente **p)
+{
+    paciente *atual, *novo, *anterior;
+	
+    int num;
+		char nome[20];
+
+    novo = (paciente *) malloc(sizeof(paciente));
+
+    atual = *p;
+    anterior = NULL;
+
+    printf("Insira um valor: ");
+    scanf("%d", &num);
+		printf("Digite um nome");
+		scanf("%s" , nome);
+
+    novo->valor = num;
+		strcpy( novo->nome, nome);
+
+    if(atual == NULL){
+        novo->prox = NULL;
+        *p = novo;
+    } else{
+      
+        while(atual != NULL && atual->valor < num){
+            anterior = atual;
+            atual = atual->prox;
+        }
+
+        novo->prox = atual;
+
+        if(anterior == NULL){
+            *p = novo;
+        } else{
+            anterior->prox = novo;
+        }
+    }
+}
+
+void mostraLista(paciente *p){
+	printf("Lista: \n");
+    while(p != NULL)
+    {
+        printf("%d \n", p->valor);
+				printf("%s \n", p->nome);
+        p = p->prox;
+    }
+}
+
+int main(void){ 
+
+	paciente *inicio = NULL;
+  char opcao = 's';
+
+	do{
+		printf(AZUL" \n \n ------------------ Menu ---------------- \n" RESET);
+		printf(" 1 - 📝Cadastrar Paciente \n 2 - 🚶Remover Paciente \n 3 - 🔍Pesquisar Paciente por nível de urgência \n");
+		printf(" 4 - 📋Visualizar fila completa \n 5 - 📋Visualizar fila por tipo de urgência \n 6 - 📋Consultar situação da fila \n");
+		printf("7 - Sair ");
+		printf("Digite uma Opção: ");
+		int op;
+		scanf("%d", &op);
+		printf(AZUL"---------------------------------------- \n \n " RESET);
+
+	switch(op){
+
+		case 1:
+			
+			   while( opcao == 's' || opcao == 'S' ){
+        	inserir(&inicio);
+
+        	printf("Deseja inserir outro paciente ? (S/N)\n");
+        	scanf(" %c", &opcao);
+    		}
+		break;
+    
+    case 4:
+			mostraLista(inicio);
+    break;
+
+    case 7:
+      printf("Saindo... \n");
+			exit(1);
+    break;
+
+    default:
+
+      printf("Digite uma opção válida \n");
+	}
+	
+	}while(controleMenu ==1);
+
+}
---
+++ READme.md
@@ -0,0 +1,44 @@
+# Sistema de Controle Hospitalar com Níveis de Prioridade
+Projeto Dia 14 <br/>
+Projeto desenvolvido durante graduação na cadeira de Estrutura de Dados
+
+ - Fluxo lógico da aplicação:
+ - sistema cadastra pacientes por níveis de prioridade.
+ - mostra pacientes por níveis de prioridade , por posição geral da fila e por posição de acordo com a sua prioridade no sistema.
+  - sistema remove da fila  primeiro os pacientes como prioridade alta (prioridade amarela), para posteriormente remover os pacientes com prioridade normal(prioridade verde).
+   <br/> 
+
+- # implementação e requisitos do sistema
+- [x] Cria um menu
+- [ ] Criar uma estrutura | criar alocação dinâmica
+- [x] Cadastrar pacientes por níveis de prioridade
+- [x] Remover pacientes da fila de acordo com o nível de prioridade seguindo a lógica FIFO
+- [x] visualizar fila completa
+- [x] visualizar fila por níveis de prioridade
+- [x] verificar estado da fila
+- [x] Pesquisar pacientes na fila de acordo com a sua prioridade na fila geral
+
+# Sistemas de cores:
+ - VERDE = "\x1b[32m"
+ - AMARELO = "\x1b[93m"
+ - VERMELHO = "\x1b[31m"
+ - AZUL = "\x1b[34m"
+ - RESET = "\x1b[0m"
+
+# Útil
+>  & - É usado em passagem por referência, posibilita na hora de editar e deletar e fazer alteração nessa variável <br/> 
+> | - É quando não é passado por referência faz uma cópia da variável e não altera o valor real da mesma.
+>Exemplo: <br/>
+> &tam
+
+# Main
+> Main (classe principal da nossa aplicação, ela que será chamada quando o código rodar) <br/> <br/>
+> int tam; - guarda total de elementos <br/>
+
+
+
+Projeto Licença MIT.
+
+Sistema desenvolvido com muito ❤️☕ por [Geordani Machado](https://github.com/Geordani-Machado) & [Gabriela Von](https://github.com/G4bizinha)
+
+- ❗ Não é gambiarra, é uma solução alternativa não oficial.
